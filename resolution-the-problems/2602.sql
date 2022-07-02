a9eca444ab38933a9e90a2332314086cbd2779d7
ramonzacarias <ramonsantana484@gmail.com>
2022-07-02 16:13:48 -0300

    alteração na estrutura do diretório

--- resolution-the-problems/2602.sql
+++ resolution-the-problems/2602.sql
@@ -2,5 +2,5 @@
 --- Resolução do desafio 2602
 
 ----------------------------------------------------------------
-SELECT name FROM customers WHERE state = 'RS'
-
+SELECT name FROM customers 
+    WHERE state = 'RS';
